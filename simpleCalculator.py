efff515c3be046270ff3471cf83acd500e7f0358
Dinith Atukorala <dinithatuko@gmail.com>
2021-05-30 21:11:08 -0400

    if statement and operations

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -1,6 +1,34 @@
-#adding 
-num1 = input("enter number : ")
-num2 = input ("enter another number: ")
-result = float(num1) + float(num2)
-print(result)
+print("select operation")
+print("1. Add")
+print("2.subtract")
+print("3.multiply")
+print("4.divide")
+operator = input()
+#adding
+if operator == "1":
+    num1 = input("enter number : ")
+    num2 = input ("enter another number: ")
+    result = float(num1) + float(num2)
+    print(result)
+#subrating 
+elif operator == "2":
+    num1 = input("enter number : ")
+    num2 = input ("enter another number: ")
+    result = float(num1) - float(num2)
+    print(result)
+#multiplying
+elif operator == "3":
+    num1 = input("enter number : ")
+    num2 = input ("enter another number: ")
+    result = float(num1) * float(num2)
+    print(result)
+#dividing
+elif operator == "4":
+    num1 = input("enter number : ")
+    num2 = input ("enter another number: ")
+    result = float(num1) // float(num2)
+    print(result)
+
+else:
+    print("invalid")
 
