302ea6b6e7064edd987f306afb83869ae0654999
Dinith Atukorala <dinithatuko@gmail.com>
2021-06-01 21:58:58 -0400

    test

--- Timers.py
+++
@@ -1,9 +0,0 @@
-import time 
-
-t=int(input("number of second you want to count down to"))
-
-while t:
-    mins = t // 60
-    secs = t % 60
-    timer = '{:02d}:{:02d}'.format(mins, secs)
-    print(timer, end = "/r")::
