51842c635c380b392a9bcb63c3998d1923f60acc
Dinith Atukorala <dinithatuko@gmail.com>
2021-07-03 17:08:49 -0400

    code

--- tictactoewithcomputer.py
+++ tictactoewithcomputer.py
@@ -115,11 +115,13 @@ def main():
     if isBoardFull(board):
         print('Tie Game!')
 
-while True:
-    answer = input('Do you want to play again? (Y/N)')
-    if answer.lower() == 'y' or answer.lower == 'yes':
-        board = [' ' for x in range(10)]
-        print('-----------------------------------')
-        main()
-    else:
-        break
+
+def hvc_main():
+    while True:
+        answer = input('Do you want to play again? (Y/N)')
+        if answer.lower() == 'y' or answer.lower == 'yes':
+            board = [' ' for x in range(10)]
+            print('-----------------------------------')
+            main()
+        else:
+            break
