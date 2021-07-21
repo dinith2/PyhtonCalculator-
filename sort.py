429af26ca78b831383594c3c142570eb81b87857
Dinith Atukorala <dinithatuko@gmail.com>
2021-07-21 15:49:11 -0400

    code

--- sort.py
+++ sort.py
@@ -70,6 +70,19 @@ def avg_main():
             except: 
                 print("invalid")
                 avg_main()
+        if answer == '2':
+            try:
+                n_num = [1, 2, 3, 4, 5]
+                n = len(n_num)
+                n_num.sort()
+  
+                if n % 2 == 0:
+                    median1 = n_num[n//2]
+                    median2 = n_num[n//2 - 1]
+                    median = (median1 + median2)/2
+                else:
+                    median = n_num[n//2]
+                    print("Median is: " + str(median))
 
 def exit 
             
