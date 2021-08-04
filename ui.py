2b866d49990c961d7d9567c6a71e3bc5da52f417
Dinith Atukorala <dinithatuko@gmail.com>
2021-08-04 15:37:10 -0400

    code

--- ui.py
+++ ui.py
@@ -1,4 +1,5 @@
 import sys
+import random
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget
 from PyQt5.QtWidgets import QPushButton
@@ -11,13 +12,26 @@ class MainWindow(QMainWindow):
         self.setMinimumSize(QSize(300, 200))    
         self.setWindowTitle("PyQt button example - pythonprogramminglanguage.com") 
 
-        pybutton = QPushButton('Click me', self)
+        pybutton = QPushButton('roll die', self)
         pybutton.clicked.connect(self.clickMethod)
         pybutton.resize(100,32)
         pybutton.move(50, 50)        
 
     def clickMethod(self):
-        print('Clicked Pyqt button.')
+        self.setMinimumSize(QSize(300, 200))
+        
+        question = input('Would you like to roll the dice [y/n]?\n')
+        while question != 'n':
+            if question == 'y':
+                die1 = random.randint(1, 6)
+                die2 = random.randint(1, 6)
+                die3 = random.randint(1, 6)
+                print(die1, die2)
+                question = input('Would you like to roll the dice [y/n]?\n')
+            else:
+                print('Invalid response. Please type "y" or "n".')
+                question = input('Would you like to roll the dice [y/n]?\n')
+                print('Clicked Pyqt button.')
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
