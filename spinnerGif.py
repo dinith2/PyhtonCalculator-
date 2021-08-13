5d0ea649ba99fd05ae66d8f1b7d2319571ff1b65
Dinith Atukorala <dinithatuko@gmail.com>
2021-08-13 16:41:20 -0400

    code

--- spinnerGif.py
+++ spinnerGif.py
@@ -8,7 +8,7 @@ class LoadingGif(object):
   
     def mainUI(self, FrontWindow):
         FrontWindow.setObjectName("FTwindow")
-        FrontWindow.resize(620, 400)
+        FrontWindow.resize(420, 400)
         self.centralwidget = QtWidgets.QWidget(FrontWindow)
         self.centralwidget.setObjectName("main-widget")
   
