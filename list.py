619a7ef7da09f20934280272a7056ba0ba790334
Dinith Atukorala <dinithatuko@gmail.com>
2021-09-06 20:53:57 -0400

    code

--- list.py
+++ list.py
@@ -8,8 +8,7 @@ List = [10, 20, 14]
 print("\nList of numbers: ")
 print(List)
   
-# Creating a List of strings and accessing
-# using index
+
 List = ["Geeks", "For", "Geeks"]
 print("\nList Items: ")
 print(List[0]) 
