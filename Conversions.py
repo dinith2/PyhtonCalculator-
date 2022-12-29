e1da02e18ddd6e391b1fda344a43422c85943836
Dinith Atukorala <dinithatuko@gmail.com>
2022-12-28 19:15:02 -0500

    not finished

---
+++ Conversions.py
@@ -0,0 +1,36 @@
+#this code is used for conversion between binary and deicmal
+
+def mainmenu():
+    while(True):
+        print ('1. bin to dec')
+        print ('2. dec to bin')
+        print('3.exit')
+        choice = int(input('> Enter your choice: '))
+
+    
+        if choice == 1:
+            print ('Starting Scan\n')
+            func()
+
+        elif choice == 2:
+            print ('Starting Ping\n')
+        elif choice == 3:
+            print ('Exiting\n')
+            exit(0)
+        else:
+            mainmenu(6)
+
+
+def binCon():
+    
+
+mainmenu()
+
+   
+
+
+
+
+
+
+
