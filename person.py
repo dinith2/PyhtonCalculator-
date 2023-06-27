808c0d777162a47f38915730eebe8edcc5fb4129
Dinith Atukorala <dinithatuko@gmail.com>
2023-06-27 14:23:38 -0400

    part1

--- person.py
+++ person.py
@@ -8,7 +8,7 @@ draw = ImageDraw.Draw(image)
 head_radius = 80
 body_height = 150
 leg_height = 150
-b
+
 head_center = (image_width // 2, head_radius)
 draw.ellipse((head_center[0] - head_radius, head_center[1] - head_radius,
               head_center[0] + head_radius, head_center[1] + head_radius),
@@ -18,12 +18,10 @@ body_top = head_center[1] + head_radius
 draw.rectangle((head_center[0] - 10, body_top, head_center[0] + 10, body_top + body_height),
                fill="lightblue", outline="black")
 
-# Draw  legs
 leg_bottom = body_top + body_height
 draw.rectangle((head_center[0] - 30, leg_bottom, head_center[0] - 10, leg_bottom + leg_height),
                fill="lightblue", outline="black")
 draw.rectangle((head_center[0] + 10, leg_bottom, head_center[0] + 30, leg_bottom + leg_height),
                fill="lightblue", outline="black")
 
-# Display the image
 image.show()
