b37c46bb250f88401e8548d23ab9ccae0af9a44e
Dinith Atukorala <dinithatuko@gmail.com>
2023-06-27 14:23:59 -0400

    part1

--- notes.py
+++ notes.py
@@ -16,7 +16,7 @@ def add_note():
     note = input("Enter your note: ")
     with open("notes.txt", "a") as file:
         file.write(note + "\n")
-    print("Note added successfully!")
+    print("Note added successfully")
 
 def delete_notes():
     clear_screen()
