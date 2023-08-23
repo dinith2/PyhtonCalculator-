deea3c5043901f89259a82fbddc4aa85e23aca65
Dinith Atukorala <dinithatuko@gmail.com>
2023-08-23 19:25:45 -0400

    1

---
+++ bts.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+
+    while queue:
+        vertex = queue.popleft()
+        print(vertex, end=" ")
+
+        for neighbor in graph[vertex]:
+            if neighbor not in visited:
+                queue.append(neighbor)
+                visited.add(neighbor)
+
+start_vertex = 'A'
+print("Breadth-First Search starting from vertex", start_vertex)
+bfs(graph, start_vertex)
