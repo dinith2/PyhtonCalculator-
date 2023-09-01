6a3bdabe5c366ffbdac590f3c1dc8b9a22b18c35
Dinith Atukorala <dinithatuko@gmail.com>
2023-08-31 22:53:13 -0400

    temp

---
+++ graphic.py
@@ -0,0 +1,90 @@
+import pygame
+import random
+import sys
+
+# Initialize pygame
+pygame.init()
+
+# Set up display
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Rock-Paper-Scissors")
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Load images
+rock_img = pygame.image.load("rock.png")
+paper_img = pygame.image.load("paper.png")
+scissors_img = pygame.image.load("scissors.png")
+
+# Game choices
+choices = ["rock", "paper", "scissors"]
+
+def draw_choice(player_choice, computer_choice):
+    screen.fill(WHITE)
+    font = pygame.font.Font(None, 36)
+
+    player_choice_img = None
+    computer_choice_img = None
+
+    if player_choice == "rock":
+        player_choice_img = rock_img
+    elif player_choice == "paper":
+        player_choice_img = paper_img
+    elif player_choice == "scissors":
+        player_choice_img = scissors_img
+
+    if computer_choice == "rock":
+        computer_choice_img = rock_img
+    elif computer_choice == "paper":
+        computer_choice_img = paper_img
+    elif computer_choice == "scissors":
+        computer_choice_img = scissors_img
+
+    screen.blit(player_choice_img, (150, 200))
+    screen.blit(computer_choice_img, (500, 200))
+
+    result_text = font.render("You " + determine_winner(player_choice, computer_choice), True, BLACK)
+    screen.blit(result_text, (300, 450))
+
+    pygame.display.update()
+
+def determine_winner(player_choice, computer_choice):
+    if player_choice == computer_choice:
+        return "tie"
+    elif (player_choice == "rock" and computer_choice == "scissors") or \
+         (player_choice == "paper" and computer_choice == "rock") or \
+         (player_choice == "scissors" and computer_choice == "paper"):
+        return "win"
+    else:
+        return "lose"
+
+def main():
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                    pygame.quit()
+                    sys.exit()
+
+                if event.key in [pygame.K_r, pygame.K_p, pygame.K_s]:
+                    player_choice = choices[event.key - pygame.K_r]
+                    computer_choice = random.choice(choices)
+                    draw_choice(player_choice, computer_choice)
+
+        pygame.display.flip()
+        clock.tick(60)
+
+if __name__ == "__main__":
+    main()
