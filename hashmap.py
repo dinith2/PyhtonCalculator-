ccc9c579530d62f2cbfc134d30cf5995b739efca
Dinith Atukorala <dinithatuko@gmail.com>
2023-12-03 21:53:00 -0500

    p1

--- hashmap.py
+++ hashmap.py
@@ -1,4 +1,41 @@
-#using hashmaps 
-pass 
-map 
-pygame.sprite.switch_layer(layer1_nr, layer2_nr)
+#using hashmap
+class HashMap:
+    def __init__(self, size=10):
+        self.size = size
+        self.map = [None] * size
+
+    def _hash(self, key):
+        return hash(key) % self.size
+
+    def add(self, key, value):
+        key_hash = self._hash(key)
+        key_value = [key, value]
+
+        if self.map[key_hash] is None:
+            self.map[key_hash] = list([key_value])
+            return True
+        else:
+            for pair in self.map[key_hash]:
+                if pair[0] == key:
+                    pair[1] = value
+                    return True
+            self.map[key_hash].append(key_value)
+            return True
+
+    def get(self, key):
+        key_hash = self._hash(key)
+        if self.map[key_hash] is not None:
+            for pair in self.map[key_hash]:
+                if pair[0] == key:
+                    return pair[1]
+        return None
+
+    def delete(self, key):
+        key_hash = self._hash(key)
+        if self.map[key_hash] is None:
+            return False
+        for i in range(len(self.map[key_hash])):
+            if self.map[key_hash][i][0] == key:
+                self.map[key_hash].pop(i)
+                return True
+        return False
