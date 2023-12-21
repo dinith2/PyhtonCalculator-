79c8658327d5c8949572195dc41242bd6aa1094f
Dinith Atukorala <dinithatuko@gmail.com>
2023-12-21 18:43:28 -0500

    1

---
+++ fibnumber.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    if n <= 0:
+        return "Input should be a positive integer."
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+# Example usage:
+n = 7
+result = fibonacci(n)
+print(f"The {n}-th Fibonacci number is: {result}")
