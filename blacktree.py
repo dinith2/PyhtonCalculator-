536adf5ad7dcf7a0c479e4bcc3fff90257e9d282
Dinith Atukorala <dinithatuko@gmail.com>
2023-12-20 19:22:42 -0500

    p1

---
+++ blacktree.py
@@ -0,0 +1,110 @@
+class Node:
+    def __init__(self, key, color, left=None, right=None, parent=None):
+        self.key = key
+        self.color = color
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+
+class RedBlackTree:
+    def __init__(self):
+        self.NIL = Node(None, "BLACK")
+        self.root = self.NIL
+
+    def insert(self, key):
+        node = Node(key, "RED", self.NIL, self.NIL, self.NIL)
+        self._insert(node)
+        self._fix_insert(node)
+
+    def _insert(self, node):
+        parent = None
+        current = self.root
+
+        while current != self.NIL:
+            parent = current
+            if node.key < current.key:
+                current = current.left
+            else:
+                current = current.right
+
+        node.parent = parent
+        if parent is None:
+            self.root = node
+        elif node.key < parent.key:
+            parent.left = node
+        else:
+            parent.right = node
+
+    def _fix_insert(self, node):
+        while node.parent.color == "RED" and node != self.root:
+            if node.parent == node.parent.parent.left:
+                uncle = node.parent.parent.right
+                if uncle.color == "RED":
+                    node.parent.color = "BLACK"
+                    uncle.color = "BLACK"
+                    node.parent.parent.color = "RED"
+                    node = node.parent.parent
+                else:
+                    if node == node.parent.right:
+                        node = node.parent
+                        self._left_rotate(node)
+                    node.parent.color = "BLACK"
+                    node.parent.parent.color = "RED"
+                    self._right_rotate(node.parent.parent)
+            else:
+                uncle = node.parent.parent.left
+                if uncle.color == "RED":
+                    node.parent.color = "BLACK"
+                    uncle.color = "BLACK"
+                    node.parent.parent.color = "RED"
+                    node = node.parent.parent
+                else:
+                    if node == node.parent.left:
+                        node = node.parent
+                        self._right_rotate(node)
+                    node.parent.color = "BLACK"
+                    node.parent.parent.color = "RED"
+                    self._left_rotate(node.parent.parent)
+
+        self.root.color = "BLACK"
+
+    def _left_rotate(self, x):
+        y = x.right
+        x.right = y.left
+        if y.left != self.NIL:
+            y.left.parent = x
+
+        y.parent = x.parent
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+
+        y.left = x
+        x.parent = y
+
+    def _right_rotate(self, y):
+        x = y.left
+        y.left = x.right
+        if x.right != self.NIL:
+            x.right.parent = y
+
+        x.parent = y.parent
+        if y.parent is None:
+            self.root = x
+        elif y == y.parent.left:
+            y.parent.left = x
+        else:
+            y.parent.right = x
+
+        x.right = y
+        y.parent = x
+
+# Example usage:
+tree = RedBlackTree()
+keys = [10, 20, 30, 15, 25, 5]
+for key in keys:
+    tree.insert(key)
